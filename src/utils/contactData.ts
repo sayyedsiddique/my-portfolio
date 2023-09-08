3fab716cc95916df863cf5c82a686d090ce5a13c
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-09-08 22:40:52 +0530

    about page data array created

---
+++ src/utils/contactData.ts
@@ -0,0 +1,13 @@
+
+import { RiComputerLine } from "react-icons/ri";
+import { Iservices } from "../types/types";
+
+
+export const services: Iservices[] = [
+  { title: "Frontend Developer", about: "when an unknown printer took a galley of type and scrambled it to make a type specimen book...", Icon: RiComputerLine },
+  { title: "Back Development", about: "when an unknown printer took a galley of type and scrambled it to make a type specimen book", Icon: RiComputerLine },
+  { title: "API Development", about: "when an unknown printer took a galley of type and scrambled it to make a type specimen book", Icon: RiComputerLine },
+  { title: "Competitive Coding", about: "when an unknown printer took a galley of type and scrambled it to make a type specimen book", Icon: RiComputerLine },
+  { title: "UI/UX Designer", about: "when an unknown printer took a galley of type and scrambled it to make a type specimen book", Icon: RiComputerLine },
+  { title: "Whatever", about: "when an unknown printer took a galley of type and scrambled it to make a type specimen book", Icon: RiComputerLine },
+];
