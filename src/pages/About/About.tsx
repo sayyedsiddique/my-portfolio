4daa039bca3a9a9d24944ac986b07fe86d71f69a
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-09-10 11:31:27 +0530

    service card comp added into about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import ServiceCard from "../../components/ServiceCard/ServiceCard";
+import { services } from "../../utils/contactData";
 
-const About:React.FC = () => {
+const About: React.FC = () => {
   return (
-    <div className='p-4 '>About</div>
-  )
-}
+    <div className="p-4 ">
+      <h5 className="mb-10 ">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur,
+        modi. Incidunt quaerat corrupti repellendus earum omnis consequuntur
+        fugiat aliquam adipisci quidem perferendis sapiente, harum obcaecati
+        doloribus! Molestiae mollitia dicta itaque.
+      </h5>
+      <div>
+        <h6 className="mb-4 ">What I Offer</h6>
 
-export default About
+        {services && services?.map((item) => {
+          return <ServiceCard title={item?.title} about={item?.about} Icon={item?.Icon} />
+        })}
+
+      </div>
+    </div>
+  );
+};
+
+export default About;
