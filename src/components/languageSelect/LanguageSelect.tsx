b91948afa9d00f4c4a5a586917426d8744694d5c
sayyedsiddique <sayyedsiddique055@gmail.com>
2024-05-05 15:11:15 +0530

    language dropdown lable removed and none value

--- src/components/languageSelect/LanguageSelect.tsx
+++ src/components/languageSelect/LanguageSelect.tsx
@@ -1,36 +1,34 @@
-import React from 'react';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { DefaultLang } from '../../types/types';
+import React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { DefaultLang } from "../../types/types";
 
 type LanguageSelectProps = {
-  languageSelectHandler: () => void,
-  selectedLang: DefaultLang
-}
-const LanguageSelect: React.FC<LanguageSelectProps> = ({languageSelectHandler, selectedLang}) => {
-  console.log("selectedLang ", selectedLang)
+  languageSelectHandler: () => void;
+  selectedLang: DefaultLang;
+};
+const LanguageSelect: React.FC<LanguageSelectProps> = ({
+  languageSelectHandler,
+  selectedLang,
+}) => {
+  console.log("selectedLang ", selectedLang);
 
-    return (
-      <FormControl sx={{ m: 1, minWidth: 80 }} size="small">
-        <InputLabel id="demo-select-small-label">Eng</InputLabel>
-        <Select
-          labelId="demo-select-small-label"
-          id="demo-select-small"
-          value={selectedLang?.name}
-          label="Age"
-          onChange={languageSelectHandler}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={"English"}>Eng</MenuItem>
-          <MenuItem value={"Hindi"}>Hin</MenuItem>
-          {/* <MenuItem value={30}>Thirty</MenuItem> */}
-        </Select>
-      </FormControl>
-    );
-}
+  return (
+    <FormControl sx={{ m: 1, minWidth: 80 }} size="small">
+      {/* <InputLabel id="demo-select-small-label">Eng</InputLabel> */}
+      <Select
+        value={selectedLang?.name}
+        onChange={languageSelectHandler}
+        inputProps={{ "aria-label": "Without label" }}
+      >
+        <MenuItem value={"English"}>Eng</MenuItem>
+        <MenuItem value={"Hindi"}>Hin</MenuItem>
+        {/* <MenuItem value={30}>Thirty</MenuItem> */}
+      </Select>
+    </FormControl>
+  );
+};
 
-export default LanguageSelect
+export default LanguageSelect;
